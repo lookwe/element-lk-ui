b3e4763841555c1e4e68d0ac3287e97ddd8b55f6
lookwe <2726768601@qq.com>
2022-04-01 18:38:29 +0800

    ui组件库项目初始化

---
+++ package/button/src/button.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="lk-buttom">
+        <button>按钮</button>
+    </div>
+</template>
+ 
+<script>
+export default {
+    name: "lkButtom",
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
+ 
+<style scoped lang="stylus"></style>
