5a284f922da4e73abe86e5f08823c956219705b5
lookwe <2726768601@qq.com>
2022-04-02 23:57:32 +0800

    input 组件构架初始

---
+++ package/input/index.ts
@@ -0,0 +1,5 @@
+import LkButtom from './src/input.vue'
+
+LkButtom.install = function (Vue) {
+    Vue.component(LkButtom.name, LkButtom)
+}
---
+++ package/input/src/input.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="lk-input">
+        <input type="text" />
+    </div>
+</template>
+ 
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class LkBotton extends Vue {}
+</script>
+ 
+<style scoped lang="stylus"></style>
