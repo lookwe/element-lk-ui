b5ec1a08d3a336f5244bf083ddc0cd48fcf71d6c
lookwe <2726768601@qq.com>
2022-04-03 23:57:43 +0800

    img图片组件 初始化

---
+++ package/image/index.ts
@@ -0,0 +1,5 @@
+import LkImage from './src/image.vue'
+
+LkImage.install = function (Vue) {
+    Vue.component(LkImage.name, LkImage)
+}
---
+++ package/image/src/image.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="lk-input">
+        <img />
+    </div>
+</template>
+ 
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class LkBotton extends Vue {}
+</script>
+ 
+<style scoped lang="stylus"></style>
